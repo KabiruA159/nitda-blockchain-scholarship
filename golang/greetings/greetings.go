88ca304aeb9fafe21f8352e326d7ea2cba38d49e
KabiruA159 <kaabdullahi12@gmail.com>
2022-12-31 11:42:07 -0800

    greetings.go

---
+++ golang/greetings/greetings.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi %v Welcome!\n", name)
+
+	return message
+}
+
+func main() {
+	fmt.Printf(Hello("Mentor"))
